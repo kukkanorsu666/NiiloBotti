ef18b0d4e723231a44c8237c51e83b17e52aaee0
kukkanorsu666 <136340728+kukkanorsu666@users.noreply.github.com>
2023-06-18 18:35:32 +0300

    Update niilo.py

--- niilo.py
+++ niilo.py
@@ -1,9 +1,8 @@
 import nextcord, os, random, scrapetube, datetime, time
 from asyncio import sleep
 from nextcord import FFmpegPCMAudio
-from nextcord import Interaction
 from nextcord.ext import tasks, commands
-
+from nextcord import Interaction
 
 intents = nextcord.Intents.all()
 intents.members = True
@@ -19,33 +18,38 @@ async def on_ready():
 SERVER_ID = SERVER ID HERE
 CHANNEL_ID = CHANNEL ID HERE
 
+
 script_dir = os.path.dirname(__file__)
 relative_path_wav = "Sound/"
 voice_path_wav = os.path.join(script_dir, relative_path_wav)
 
 
+
 #RANDOM LUIKAUS
-def luikaus(luikaukset):
+def luikaus():
     random_luikaus = open("luikaukset.txt", encoding='utf-8').read().splitlines()
     return random.choice(random_luikaus)
 
 #RANDOM VIDEO
-def video(videot):
+def video():
     random_video = open('videot.txt', encoding='utf-8').read().splitlines()
     return random.choice(random_video)
 
 #LUIKAUSKOMENTO
 @client.slash_command(name = "luikaus", description = "Ei siitä sen enempää", guild_ids=[SERVER_ID])
-async def testcommand(interaction: Interaction):
-	await interaction.response.send_message(luikaus(luikaus))
+async def luikauscom(interaction: Interaction):
+	await interaction.response.send_message(luikaus())
 
 #RANDOM VIDEOKOMENTO
 @client.slash_command(name = "video", description = "Tästä videoustia", guild_ids=[SERVER_ID])
-async def testcommand(interaction: Interaction):
-	await interaction.response.send_message((video(video)))
+async def videocom(interaction: Interaction):
+	await interaction.response.send_message((video()))
 
 #RANDOM ÄÄNIKLIPPI
-def random_voice(voice_):
+def random_voice():
+	script_dir = os.path.dirname(__file__)
+	relative_path_wav = "Sound/"
+	voice_path_wav = os.path.join(script_dir, relative_path_wav)
 	random_voice = random.choice(os.listdir(voice_path_wav))
 	print("playing -- " + random_voice)
 	return random_voice
@@ -53,10 +57,11 @@ def random_voice(voice_):
 #CONNECT VOICE
 @client.command(pass_context = True)
 async def n(ctx):
+	print("moi")
 	if(ctx.author.voice):
 		channel = ctx.message.author.voice.channel
 		voice = await channel.connect()
-		chosen_voice = voice_path_wav + random_voice(random_voice)
+		chosen_voice = voice_path_wav + random_voice()
 		source = FFmpegPCMAudio(chosen_voice)
 		player = voice.play(source)
 		while voice.is_playing():
@@ -65,6 +70,7 @@ async def n(ctx):
 
 	else:
 		await ctx.send("Mee kanavalle saatana")
+
 
 @client.command(pass_context = True)
 async def leave(ctx):
@@ -85,6 +91,8 @@ async def daily_loop():
 	x = datetime.time(16, 00)
 	schedule_time_hour = x.hour
 	schedule_time_minute = x.minute
+
+
 	if schedule_time_hour  == datetime.datetime.now().hour and schedule_time_minute == datetime.datetime.now().minute:
 		channel = client.get_channel(CHANNEL ID HERE)
 		await channel.send(daily())
