9a6f67a9a6f8ab186476e49a22c064693f24a71c
kukkanorsu666 <136340728+kukkanorsu666@users.noreply.github.com>
2025-11-14 06:29:07 +0200

    Refactor main.py to modularize code structure
    Refactor main.py by separating imports, commands, and tasks into their respective modules for better organization and maintainability.

---
+++ main.py
@@ -0,0 +1,16 @@
+from imports import *
+from config import BOT_TOKEN
+from tasks import setup_tasks
+from commands import setup_commands
+
+intents = disnake.Intents.all()
+client = commands.Bot(command_prefix="!", intents=intents)
+
+@client.event
+async def on_ready():
+	print("Valmis")
+
+setup_commands(client)
+setup_tasks(client)
+
+client.run(BOT_TOKEN)
